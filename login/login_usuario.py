fa7dd2df3ee37e60d8879c60ad773bdf167c8f7e
Anibal Ezequiel Carranza <ezequielcarranza02@gmail.com>
2021-10-23 21:41:00 -0300

    cambios 2

--- login/login_usuario.py
+++ login/login_usuario.py
@@ -1,44 +1,41 @@
 from tkinter import *
 
-datos={}
+X_LABEL, X_ENTRYBOX, Y_USUARIO, Y_CLAVE = 12, 110, 11, 41
+COLOR_VENTANA, COLOR_FRAME, COLOR_FUENTE = "#574236", "#C3581B", "white"
 
-def obtener_usuario_claves():
-    global datos
-    datos={"Richard":"Internautas", "Francisco":"Internautas", "Ezequiel":"Internautas", "Rocio":"Internautas", "Jaime":"Internautas", "Nicolas":"Internautas"}
-    print(datos)
+def obtener_datos():
+    datos = {"Richard":"1", "Francisco":"2", "Ezequiel":"3", "Rocio":"4", "Jaime":"5", "Nicolas":"6"}
+    return datos
 
 def levantar_validacion(mensaje):
     #popup
     ventana_validacion = Toplevel(ventana)
     ventana_validacion.title("Validación")
     ventana_validacion.geometry("257x28")
-    ventana_validacion.config(bg="#574236")
-    ventana_validacion.resizable(0,0)
+    ventana_validacion.config(bg=COLOR_VENTANA)
+    ventana_validacion.resizable(0, 0)
     ventana_validacion.iconbitmap("internautas.ico")
     #label
-    texto = Label(ventana_validacion,text=mensaje,fg="white")
-    texto.config(bg="#574236")
-    texto.place(x=5,y=2)
-    '''
-    validacion.config(text="")
-    validacion.config(text=mensaje)
-    validacion.place(x=6, y=3)
-    '''
+    texto = Label(ventana_validacion, text=mensaje, fg=COLOR_FUENTE)
+    texto.config(bg=COLOR_VENTANA)
+    texto.place(x=5, y=2)
 
 def validar_datos():
     usuario = texto_usuario.get()
     clave = texto_clave.get()
     if usuario and clave:
-        usuario_valido = False
-        for dato in datos:
-            if dato == usuario and datos[dato] == clave:
+        datos = obtener_datos()
+        usuarios = list(datos.keys())
+        claves = list(datos.values())
+        if usuario in usuarios:
+            pos = usuarios.index(usuario)
+            if clave == claves[pos]:
                 levantar_validacion("Usuario y Clave Correctos")
-                usuario_valido = True
-        if not usuario_valido:
+            else:
+                levantar_validacion("Algunos de los datos ingresados es Incorrecto")
+        else:
             levantar_validacion("Algunos de los datos ingresados es Incorrecto")
 
-
-obtener_usuario_claves()
 
 #ventana    
 ventana=Tk()
@@ -46,28 +43,28 @@ ventana.title("Login Internautas")
 ventana.geometry("300x130")
 ventana.resizable(0,0)
 ventana.iconbitmap("internautas.ico")
-ventana.config(bg="#574236")
+ventana.config(bg=COLOR_VENTANA)
 
 #frame
 miframe=Frame(ventana,width=240,height=70)
-miframe.config(bg="#C3581B")
+miframe.config(bg=COLOR_FRAME)
 miframe.pack()
 miframe.place(x=30,y=30)
 
 #entrada de datos
-usuario=Label(miframe,text="Usuario Alumno",fg="white")
-usuario.config(bg="#C3581B")
-usuario.place(x=12, y=11)
+usuario=Label(miframe,text="Usuario Alumno",fg=COLOR_FUENTE)
+usuario.config(bg=COLOR_FRAME)
+usuario.place(x=X_LABEL,y=Y_USUARIO)
 
 texto_usuario=Entry(miframe)
-texto_usuario.place(x=110,y=11)
+texto_usuario.place(x=X_ENTRYBOX,y=Y_USUARIO)
 
-clave=Label(miframe,text="Clave",fg="white")
-clave.config(bg="#C3581B")
-clave.place(x=12, y=41)
+clave=Label(miframe,text="Clave",fg=COLOR_FUENTE)
+clave.config(bg=COLOR_FRAME)
+clave.place(x=X_LABEL,y=Y_CLAVE)
 
 texto_clave=Entry(miframe)
-texto_clave.place(x=110,y=41)
+texto_clave.place(x=X_ENTRYBOX,y=Y_CLAVE)
 texto_clave.config(show="*")
 
 validacion = Label(miframe)
